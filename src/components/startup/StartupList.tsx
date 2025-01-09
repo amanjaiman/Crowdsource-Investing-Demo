8d3fee5afbb7bb8d92e222665d12282c351e78f9
Aman Jaiman <amanjaiman@outlook.com>
2025-01-09 18:05:24 -0500

    Update StartupList.tsx

--- src/components/startup/StartupList.tsx
+++ src/components/startup/StartupList.tsx
@@ -445,6 +445,7 @@ export function StartupList({ startups, onVote }: StartupListProps) {
                                 style: "currency",
                                 currency: "USD",
                                 notation: "compact",
+                                maximumFractionDigits: 1,
                               }).format(
                                 selectedStartup.fundraising.targetAmount /
                                   (selectedStartup.fundraising.equity / 100)
